97bdde355c27f1cd1ad90310766cb212840e98da
DICSA <belovrl1@gmail.com>
2022-03-03 22:08:12 +0300

    Ячейка + Презент

---
+++ Test/Presenter.swift
@@ -0,0 +1,15 @@
+//
+//  Presenter.swift
+//  Test
+//
+//  Created by Белов Руслан on 03.03.2022.
+//
+
+import Foundation
+import UIKit
+
+
+class Presenter {
+    var text = "Hello Hell"
+    var color = UIColor(red: 255/255, green: 204/255, blue: 0/255, alpha: 1)
+}
--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    var presenter = Presenter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        label.text = presenter.text
+
         // Do any additional setup after loading the view.
     }
-
-
+    
 }
+
 
